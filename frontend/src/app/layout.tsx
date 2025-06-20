d04bcb458010d2e43e5734729c1e870aa0e6950b
Abdullahi <djayableez@gmail.com>
2025-06-20 20:55:54 +0000

    Update dependencies and enhance layout with ThemeProvider integration

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "#/styles/globals.css";
 import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 import { TRPCReactProvider } from "../trpc/react";
+import { ThemeProvider } from "#/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "NumbersPay",
@@ -24,7 +25,16 @@ type RootLayoutProps = Readonly<{
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
   <html lang="en" className={geist.variable}>
     <body>
-      <TRPCReactProvider>{children}</TRPCReactProvider>
+      <TRPCReactProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          forcedTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
+      </TRPCReactProvider>
     </body>
   </html>
 );
